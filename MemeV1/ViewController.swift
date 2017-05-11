870604953b3c70a9924c64e99e785ca1c04efac6
divadracarys <diva.kabra@gmail.com>
2017-05-11 12:06:14 +0530

    First commit: Adding layout of the elements

---
+++ MemeV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeV1
+//
+//  Created by Divya Kabra on 5/11/17.
+//  Copyright © 2017 Divya Kabra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
