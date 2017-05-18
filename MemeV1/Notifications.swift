b9e3b59fb1c1656140a3ed476bd7a0f0bff45873
divadracarys <diva.kabra@gmail.com>
2017-05-19 00:34:33 +0530

    Shifted Notifications to an extension file

---
+++ MemeV1/Notifications.swift
@@ -0,0 +1,46 @@
+//
+//  Notifications.swift
+//  MemeV1
+//
+//  Created by Divya Kabra on 5/19/17.
+//  Copyright © 2017 Divya Kabra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController {
+    // MARK: Sign Up For Keyboard Notifications
+    // Get notified when the keyboard appears
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+        
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    
+    // When the keyboardWillShow notification is received, shift the view's frame up
+    func keyboardWillShow(_ notification:Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    // Now to move the view back down
+    func keyboardWillHide(_ notification: Notification){
+        view.frame.origin.y = 0
+        // view.frame.origin.y += getKeyboardHeight(notification)
+    }
+
+}
--- MemeV1/ViewController.swift
+++ MemeV1/ViewController.swift
@@ -80,38 +80,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
-    // MARK: Sign Up For Keyboard Notifications
-    // Get notified when the keyboard appears
-    func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
-        
-    }
     
-    func unsubscribeFromKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
-    }
-    
-    
-    // When the keyboardWillShow notification is received, shift the view's frame up
-    func keyboardWillShow(_ notification:Notification) {
-        if bottomTextField.isFirstResponder {
-         view.frame.origin.y -= getKeyboardHeight(notification)   
-        }
-    }
-    
-    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
-        return keyboardSize.cgRectValue.height
-    }
-    
-    // Now to move the view back down
-    func keyboardWillHide(_ notification: Notification){
-        view.frame.origin.y = 0
-        // view.frame.origin.y += getKeyboardHeight(notification)
-    }
-
 }
 
